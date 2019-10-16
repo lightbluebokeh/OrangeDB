a9944477f10a2fb64bf61363f077f3b34d70fe9d
function2 <function2@qq.com>
2019-10-17 00:11:14 +0800

    write buf

--- dbms/record/bytes.h
+++ dbms/record/bytes.h
@@ -3,14 +3,31 @@
 #include <defs.h>
 #include <string>
 #include <cstring>
+#include <type_traits>
 
 using Bytes = std::basic_string<byte_t>;
+template<class T>
+constexpr bool is_bytable_v = std::disjunction_v<std::is_integral<T>, 
+                                                std::is_floating_point<T>, 
+                                                std::is_same<T, Bytes>>;
 
-void write_buf(buf_t buf, const Bytes& bytes, size_t n) {
-    memcpy(buf, bytes.data(), n);
-    memset(buf + bytes.size(), 0, n - bytes.size());
+template<typename T>
+void write_buf(buf_t& buf, const std::enable_if_t<is_bytable_v<T>, T>& t, size_t n) {
+    assert(sizeof(T) <= n);
+    memcpy(buf, &t, sizeof(T));
+    memset(buf + sizeof(T), 0, n - sizeof(T));
+    buf += n;
 }
 
-void write_buf(buf_t buf, const Bytes& bytes) {
-    write_buf(buf, bytes, bytes.size());
+template<>
+void write_buf<Bytes>(buf_t& buf, const Bytes& bytes, size_t n) {
+    assert(bytes.size() <= n);
+    memcpy(buf, bytes.data(), n);
+    memset(buf + bytes.size(), 0, n - bytes.size());
+    buf += n;
+}
+
+template<typename T>
+void write_buf(buf_t& buf, const std::enable_if_t<is_bytable_v<T>, T>& t) {
+    write_buf(buf, t, sizeof(T));
 }
--- dbms/record/file.h
+++ dbms/record/file.h
@@ -9,44 +9,38 @@
 #include "fs/utils/pagedef.h"
 #include <record/filed_def.h>
 #include <fs/bufmanager/BufPageManager.h>
+#include <record/bytes.h>
 
 class File {
 private:
     int id;
     std::string name;
-    // name and type
 
     int record_size, record_count;
     std::vector<FieldDef> fields;
 
+    // name and type
     void init_metadata(const std::vector<std::pair<String, String>>& name_type_list) {
         assert(name_type_list.size() <= MAX_COL_NUM);
         fields.clear();
         int page_id;
         auto buf = BufPageManager::get_instance()->getPage(id, 0, page_id);
-        memset(buf, name_type_list.size(), 1);
+        // memset(buf, name_type_list.size(), 1);
         buf++;
         for (auto &name_type: name_type_list) {
             auto field = FieldDef::parse(name_type);
-            auto bytes = field.to_bytes();
-            memcpy(buf, bytes.data(), bytes.length());
-            memcpy(buf, bytes.data() + bytes.length(), COL_SIZE - bytes.length());
+            // write_buf<Bytes>(buf, field.to_bytes(), COL_SIZE);
+            // write_buf()
+            // write_buf(buf, )==
+            // auto bytes = field.to_bytes();
+            // memcpy(buf, bytes.data(), bytes.length());
+            // memcpy(buf, bytes.data() + bytes.length(), COL_SIZE - bytes.length());
             fields.push_back(std::move(field));
             buf += COL_SIZE;
         }
-        assert(fields.size() <= MAX_COL_NUM);
-        buf++;
-
-        // for (int i = 0; i < MAX_COL_NUM; i++, buf += COL_SIZE) {
-        //     if (i < fields.size()) {
-        //         auto &field = fields[i];
-        //         auto bytes = field.to_bytes();
-        //         memcpy(buf, bytes.data(), bytes.length());
-        //         memcpy(buf, bytes.data() + bytes.length(), COL_SIZE - bytes.length());
-        //     } else {
-        //         memset(buf, 0, COL_SIZE);
-        //     }
-        // }
+        memset(buf, 0, COL_SIZE * (MAX_COL_NUM - fields.size()));
+        buf += COL_SIZE * (MAX_COL_NUM - fields.size());
+        
     }
 
     void load_metadata() {
