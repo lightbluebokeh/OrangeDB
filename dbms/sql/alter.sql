7d771f3ec22f39c44d09ac185d886c1e3d76b39d
function2 <function2@qq.com>
2020-01-10 00:31:22 +0800

    suishou

--- dbms/sql/alter.sql
+++ dbms/sql/alter.sql
@@ -1,1 +1,5 @@
-alter table test add ddd
+alter table test add ddd;
+alter table test add index test(a);
+alter table test drop index test;
+alter table test add primary key(a, b);
+alter table test drop primary key;
